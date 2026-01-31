290f789e59ccb8bd6ce70413d5355f52daa7b3d1
satv2025 <traplegionrecords2020@gmail.com>
2026-01-31 03:18:29 -0300

    Update operation.js

--- assets/js/operation.js
+++ assets/js/operation.js
@@ -4,7 +4,7 @@
 
 const sb = window.supabase.createClient(
     "https://api.solargentinotv.com.ar",
-    "TU_ANON_KEY"
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNwemd4dmtlZHNkampoenp5eXNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk1MzQwOTAsImV4cCI6MjA4NTExMDA5MH0.RgFghlZVV4Ww27rfh96nTiafDwRu9jtC3S6Y6aFdIxE"
 );
 
 const $ = id => document.getElementById(id);
